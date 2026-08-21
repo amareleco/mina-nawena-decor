2ca52ccff87427ef9d413e646112f82765491050
amareleco <albinomareleco3@gmail.com>
2026-08-21 15:24:43 +0200

    configure production cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,10 +37,7 @@ async def global_exception_handler(request, exc):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "http://127.0.0.1:3000",
-        "http://localhost:3000",
+        "https://mina-nawena-decor-1.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
